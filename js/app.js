975b16e17d8d0a749899e7b152cb44d6bbee3b42
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-09-19 14:08:47 -0700

    First Commit-basics of html and started Jss

---
+++ js/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var userName = prompt('I\'m Nicholas, what is your name?');
+alert('Hi, ' + userName + ', nice to meet you');
+console.log ('userName is ' + userName);
+
+var answer1 = prompt('Does Nicholas Have a dog');
+if(yes) {
+alert('correct')
+} else{
+alert('Nope, ')
+}
+
+// Does Nicholas Have a dog</li>
+// <li>Is Nicholas from Washington</li>
+// <li>Did Nicholas work for law enforcement?</li>
+// <li>Does Nicholas speak French?</li>
+// <li>Did Nicholas climb K2</li>
+//
