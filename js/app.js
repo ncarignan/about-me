0d4aac52d4967c881cc39e8eed7ce5a0fbe7967b
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-09-19 14:36:38 -0700

    Second Commit, Four js questions complete and working

--- js/app.js
+++ js/app.js
@@ -1,15 +1,45 @@
 'use strict';
 
-var userName = prompt('I\'m Nicholas, what is your name?');
-alert('Hi, ' + userName + ', nice to meet you');
-console.log ('userName is ' + userName);
+// var userName = prompt('I\'m Nicholas, what is your name?');
+// alert('Hi, ' + userName + ', nice to meet you');
+// console.log ('userName is ' + userName);
 
-var answer1 = prompt('Does Nicholas Have a dog');
-if(yes) {
-alert('correct')
+// var answer1 = prompt('Does Nicholas Have a dog').toLowerCase();
+// console.log('answer 1 is', answer1);
+// if(answer1 === 'y' || answer1 === 'yes') {
+// alert('Correct, he actually has two dogs: a black lab and a german shepherd');
+// } else{
+// alert('Incorrect, he actually has two dogs: a black lab and a german shepherd');
+// }
+
+var answer2 = prompt('Is Nicholas from Washington?').toLowerCase();
+if(answer2 === 'y' || answer2 === 'yes') {
+alert('Incorrect, Nicholas grew up in California and moved to the mountains of Idaho, he only just moved to Seattle.');
 } else{
-alert('Nope, ')
+alert('Correct, Nicholas grew up in California and moved to the mountains of Idaho, he only just moved to Seattle.');
 }
+
+var answer3 = prompt('Did Nicholas come from law enforcement?').toLowerCase();
+if(answer3 === 'y' || answer3 === 'yes') {
+alert('correct, Nicholas was a 911 disparcher for almost 3 years, he propably has some interesting stories.');
+} else{
+alert('Incorrect, Nicholas was a 911 disparcher for almost 3 years, he propably has some interesting stories.');
+}
+
+var answer4 = prompt('Does Nicholas speak French?').toLowerCase();
+if(answer4 === 'y' || answer4 === 'yes') {
+alert('Incorrect, Nicholas can read and write a little Mandarin but does not know any French.');
+} else{
+alert('Correct, Nicholas can read and write a little Mandarin but does not know any French');
+}
+
+var answer5 = prompt('Does Nicholas speak French?').toLowerCase();
+if(answer5 === 'y' || answer5 === 'yes') {
+alert('Incorrect, however, Nicholas has ');
+} else{
+alert('Correct, Nicholas can read a d write a little Mandarin but does not know any French');
+}
+
 
 // Does Nicholas Have a dog</li>
 // <li>Is Nicholas from Washington</li>
